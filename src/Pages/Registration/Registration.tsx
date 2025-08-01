22f50bd0bc0a99f21a600487e8df8c9aaad04de2
DD155 <dd155@protonmail.com>
2025-08-01 14:34:51 -0400

    Add typing changes

--- src/Pages/Registration/Registration.tsx
+++ src/Pages/Registration/Registration.tsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
 const Registration: React.FC = () => {
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
-    const [isRegister, setIsRegister] = useState(true)
+    const [email, setEmail] = useState<string>('')
+    const [pass, setPass] = useState<string>('')
+    const [isRegister, setIsRegister] = useState<boolean>(true)
 
     return (
         <div>
             <div className="flex items-center justify-center h-screen bg-gray-100">
-                <div className="flex flex-col w-full min-h-[45%] max-w-md p-8 bg-white rounded shadow-lg">
-                    <h2 className='text-2xl font-bold mb-1 text-center'> {isRegister ? 'Register' : 'Login'}</h2>
+                <div className="flex flex-col w-full min-h-[40%] max-w-md p-8 bg-white rounded shadow-lg">
+                    <h2 className='text-2xl font-bold mb-10 text-center'> {isRegister ? 'Register' : 'Login'}</h2>
                     <div className='flex-1 flex flex-grow flex-col justify-center'>
-                        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className='lg:p-2  w-full border rounded '/> <br/>
-                        <input type='text' placeholder = "Password" value={pass} onChange={(e) => setPass(e.target.value)} className='lg:p-2 w-full border rounded mb-4'/>
+                        <input type="email" placeholder="Email" value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} className='lg:p-2  w-full border rounded '/> <br/>
+                        <input type='text' placeholder = "Password" value={pass} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPass(e.target.value)} className='lg:p-2 w-full border rounded mb-4'/>
                         <button className='w-full bg-black text-white py-2 mb-4 border rounded '> { isRegister ? 'Register' : 'Login' } </button>
                         <div>
                             <h6 className='inline'> { isRegister ? 'Already have an account?' : 'Need a new account?' } </h6> 
@@ -23,17 +23,6 @@ const Registration: React.FC = () => {
                     </div>
                 </div>
             </div>
-            
-            {/*
-            <div className="flex items-center justify-center h-screen bg-gray-100">
-                <div className="w-full max-w-sm p-6 bg-white rounded shadow">
-                    {/*<h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
-                    <input type="email" placeholder="Email" className="w-full mb-3 p-2 border rounded" />
-                    <input type="password" placeholder="Password" className="w-full mb-3 p-2 border rounded" />
-                    <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Sign In</button>
-                </div>
-            </div>
-            */}
         </div>)
 };
 
